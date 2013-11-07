601a5cfe77d3ff4768d06a128102f84f11214bc8
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-07 14:43:48 -0500

    Created a basic login page.

--- views.py
+++ views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_view(request):
     return render(request,"scheduler/base.html")
 def login_view(request):
-    pass
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+        return HttpResponse("Logged in successfully.")
+    else:
+        return render(request,"scheduler/login.html", {"form":form})
 def logout_view(request):
     logout(request)
     return render(request, "scheduler/loggedout.html")
