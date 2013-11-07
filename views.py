46e17476a398097033caeb95aa602c8fad035c73
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-07 11:11:37 -0500

    Added a few basic views for home, login, logout and urls to reference some of them.

--- views.py
+++ views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.contrib.auth import logout
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request,"scheduler/base.html")
+def login_view(request):
+    pass
+def logout_view(request):
+    logout(request)
+    return render(request, "scheduler/loggedout.html")
