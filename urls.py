28423d05d2c54f269d7f47b341a29240b248b824
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-07 11:12:42 -0500

    Added login_view to urls.py

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
         url(r'^$','scheduler.views.home_view'),
+        url(r'^login$','scheduler.views.login_view'),
         url(r'^logout$','scheduler.views.logout_view'),
-                       )
+        )
