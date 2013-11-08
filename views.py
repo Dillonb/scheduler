0d311d7808891adc97c6cedaf7856b5b3a642db2
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-08 13:00:05 -0500

    Spacing!

--- views.py
+++ views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request,"scheduler/base.html")
+
 def login_view(request):
     form = AuthenticationForm(data=request.POST)
     if form.is_valid():
@@ -22,6 +23,7 @@ def login_view(request):
         return HttpResponse("Logged in successfully.")
     else:
         return render(request,"scheduler/login.html", {"form":form})
+
 def logout_view(request):
     logout(request)
     return render(request, "scheduler/loggedout.html")
