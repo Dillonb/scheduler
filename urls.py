7a53c14f08387ae1a3263c3e80815d97212411f5
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-08 12:22:33 -0500

    Actually fixed login and created a /is_loggedin page

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
         url(r'^$','scheduler.views.home_view'),
         url(r'^login$','scheduler.views.login_view'),
         url(r'^logout$','scheduler.views.logout_view'),
+        url(r'^is_loggedin','scheduler.views.is_loggedin_view'),
         )
--- views.py
+++ views.py
@@ -9,19 +9,25 @@ def home_view(request):
 def login_view(request):
     form = AuthenticationForm(data=request.POST)
     if form.is_valid():
-        user = authenticate(username=form.username, password=form.password)
-        if user is not None:
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password) 
+        if user:
             if user.is_active:
                 login(request,user)
-                return HttpResponse("Logged in successfully.")
             else:
-                pass
-                #TODO: Account disabled error page
+                return HttpResponse("User not active.")
         else:
-            pass
-            #TODO: Login invalid error page
+            return HttpResponse("User invalid.")
+        return HttpResponse("Logged in successfully.")
     else:
         return render(request,"scheduler/login.html", {"form":form})
 def logout_view(request):
     logout(request)
     return render(request, "scheduler/loggedout.html")
+
+def is_loggedin_view(request):
+    if request.user.is_authenticated():
+        return HttpResponse("You are logged in.")
+    else:
+        return HttpResponse("You are not logged in.")
