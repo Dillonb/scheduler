753d1fc8eaed7abde572f896327f43bce4e8e590
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-08 07:36:50 -0500

    Made login page actually log user in

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -8,7 +9,17 @@ def home_view(request):
 def login_view(request):
     form = AuthenticationForm(data=request.POST)
     if form.is_valid():
-        return HttpResponse("Logged in successfully.")
+        user = authenticate(username=form.username, password=form.password)
+        if user is not None:
+            if user.is_active:
+                login(request,user)
+                return HttpResponse("Logged in successfully.")
+            else:
+                pass
+                #TODO: Account disabled error page
+        else:
+            pass
+            #TODO: Login invalid error page
     else:
         return render(request,"scheduler/login.html", {"form":form})
 def logout_view(request):
