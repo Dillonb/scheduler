05db15cd60f3d90d2c8fc28cb9cfcf37f2aeb1eb
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-09 09:50:44 -0500

    Moved views from /login to /accounts/login, /logout to /accounts/logout, etc

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
         url(r'^$','scheduler.views.home_view'),
-        url(r'^login$','scheduler.views.login_view'),
-        url(r'^logout$','scheduler.views.logout_view'),
-        url(r'^is_loggedin','scheduler.views.is_loggedin_view'),
+        url(r'^accounts/login$','scheduler.views.login_view'),
+        url(r'^accounts/logout$','scheduler.views.logout_view'),
+        url(r'^accounts/is_loggedin','scheduler.views.is_loggedin_view'),
         )
