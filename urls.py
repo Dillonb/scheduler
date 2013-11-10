7100bb7bee81ecf7f04bfe4e1eca3e160eda4431
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-10 10:38:24 -0500

    Fixed some urls

--- urls.py
+++ urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
         url(r'^$','scheduler.views.home_view'),
         url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html'}),        
-        url(r'^accounts/logout$','scheduler.views.logout_view'),
-        url(r'^accounts/is_loggedin$','scheduler.views.is_loggedin_view'),
-        url(r'^accounts/register$','scheduler.views.register_view'),
-        url(r'^accounts/profile$','scheduler.views.account_view'),
+        url(r'^accounts/logout/$','scheduler.views.logout_view'),
+        url(r'^accounts/is_loggedin/$','scheduler.views.is_loggedin_view'),
+        url(r'^accounts/register/$','scheduler.views.register_view'),
+        url(r'^accounts/profile/$','scheduler.views.account_view'),
         url(r'^createevent/(?P<scheduleid>\d)$','scheduler.views.create_event_view'),
         )
