238cc65967c3ca00a6869f5d1cfbe8a46a28eb03
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-10 09:23:01 -0500

    Renamed /account to /accounts/profile to work with django's auth system better.

--- urls.py
+++ urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
         url(r'^accounts/logout$','scheduler.views.logout_view'),
         url(r'^accounts/is_loggedin$','scheduler.views.is_loggedin_view'),
         url(r'^accounts/register$','scheduler.views.register_view'),
-        url(r'^account$','scheduler.views.account_view'),
+        url(r'^accounts/profile$','scheduler.views.account_view'),
         url(r'^createevent$','scheduler.views.create_event_view'),
         )
