89d811a1190ed64d59013184177cdb04797c416c
Dillon Beliveau <dillonbeliveau@gmail.com>
2013-11-10 09:56:21 -0500

    Made event creating interface actually work

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 
 from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
 from scheduler.forms import EventForm
@@ -38,13 +38,17 @@ def account_view(request):
     return render(request, "scheduler/account.html",{'schedules':schedules})
     
 @login_required
-def create_event_view(request):
-    form = EventForm(data=request.POST)
+def create_event_view(request,scheduleid):
+    #TODO: Add a check to make sure that the user owns this schedule
+    schedule = get_object_or_404(Schedule, id=scheduleid) # The parent schedule
+    form = EventForm(data=request.POST) # Load the form
     if request.method == 'POST':
         if form.is_valid():
             event = form.save(commit = False) # Get an event with the form data (don't commit to db yet)
-            #TODO: Make this link to a parent schedule somehow
+            event.schedule = schedule # Set the parent schedule
+            event.save() # NOW we can save to the database.
+            return redirect("/accounts/profile") #TODO: make this redirect somewhere good
         else:
-            return render(request, "scheduler/createevent.html",{'form':form})
+            return render(request, "scheduler/createevent.html",{'form':form,'scheduleid':scheduleid})
     else:
-        return render(request, "scheduler/createevent.html",{'form':form})
+        return render(request, "scheduler/createevent.html",{'form':form,'scheduleid':scheduleid})
