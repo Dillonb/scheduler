eec084b831656a6ef5ff4b38802d10542e230830
brasky <redx47@gmail.com>
2013-12-01 21:53:50 -0500

    im mad

--- urls.py
+++ urls.py
@@ -19,6 +19,4 @@ urlpatterns = patterns('',
         url(r'^editschedule/(?P<scheduleid>\d+)$','scheduler.views.edit_schedule_view'),
         url(r'^editevent/(?P<eventid>\d+)$','scheduler.views.edit_event_view'),
         url(r'^setmainschedule/(?P<scheduleid>\d+)$','scheduler.views.set_main_schedule_view'),
-
-
         )
