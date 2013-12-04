922c305e752e5061a5cea73a2a250b4f8943b836
brasky <redx47@gmail.com>
2013-12-03 19:23:24 -0500

    Fixed the register view mistake
    I am dum.

--- views.py
+++ views.py
@@ -27,7 +27,7 @@ def is_loggedin_view(request):
         return HttpResponse("You are not logged in.")
 
 def register_view(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated():
         return redirect('/')
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
