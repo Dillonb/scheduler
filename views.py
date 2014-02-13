a4207e723dc88f43868daa75a4905e9e859c282d
Dillon Beliveau <dillonbeliveau@gmail.com>
2014-02-12 19:04:58 -0500

    (very small) beginnings of an API

--- views.py
+++ views.py
@@ -323,10 +323,13 @@ def delete_schedule_view(request, scheduleid):
             schedule.delete()
     return redirect("/accounts/profile/")
 
-def stylesheet_view(request,styleid):
+def stylesheet_view(request, styleid):
     styleid = int(styleid)
     if styleid == 0:
         return render(request, "scheduler/basestyle.css", content_type="text/css")
     if styleid == 1:
         return render(request, "scheduler/schedulestyle.css", content_type="text/css")
     raise Http404
+
+def api_view(request, apikey, format, commandrequested):
+    pass
