3e7aee3088cf67fac9c19d21958512b78a08ff4c
Dillon Beliveau <dillonbeliveau@gmail.com>
2014-04-28 23:48:22 -0400

    Also added it on schedule_compare_view

--- views.py
+++ views.py
@@ -132,7 +132,7 @@ def schedule_compare_view(request, otheruserid, viewinguserid, starttime=None):
 
         days.append((day, busy_times))
 
-    return render(request, "scheduler/schedule.html", {'events':days, 'starttime':starttime,'otheruser':user1, 'viewinguser':user2, 'comparing':True})
+    return render(request, "scheduler/schedule.html", {'events':days, 'now':datetime.datetime.now(), 'starttime':starttime,'otheruser':user1, 'viewinguser':user2, 'comparing':True})
 
 def schedule_view(request, scheduleid, view=0, starttime=None):
     if starttime == None:
